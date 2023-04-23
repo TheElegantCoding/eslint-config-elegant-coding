858587217aa6a0bce3ff00080775267f6f0b70da
Luis Monsalve <luismonsalve1941@gmail.com>
2023-04-23 12:32:05 -0400

    :sparkles: feat: validate commit with got hooks

---
+++ script/asset/icon.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ICON_GEAR=' \uf085 '
+ICON_CHECK=' \uf00c '
+ICON_ERROR=' \uf00d '
+ART=' 🎨 '
+PERFORMANCE=' ⚡️ '
+FEATURE=' ✨ '
+
+TYPES_ICONS=(
+'✨ feat'
+'🐛 fix'
+'📝 doc'
+'🎨 style'
+'♻️  refactor'
+'⚡ perf'
+'🧪 test'
+'👷 ci'
+'➕ add'
+'➖ remove'
+'🚀 realease'
+'🚨 breaking'
+'🔧 config'
+'🔒️ security'
+'🌐 locale'
+'🎉 begin'
+'✏️  type'
+'🍱 asset'
+'📦️ package')

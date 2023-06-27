b3834f1b20e17607d452ae582225afdc6faf84cd
AesthetiCoder <luismonsalve1941@gmail.com>
2023-06-26 22:59:50 -0400

    :sparkles: feat: qwik module

---
+++ src/module/qwik/rule/qwik_general_rule.ts
@@ -0,0 +1,14 @@
+const qwikGeneralRule =
+{
+  'qwik/use-method-usage': 'error',
+  'qwik/valid-lexical-scope': 'error',
+  'qwik/no-react-props': 'error',
+  'qwik/prefer-classlist': 'error',
+  'qwik/jsx-no-script-url': 'error',
+  'qwik/loader-location': 'error',
+  'qwik/jsx-key': 'error',
+  'qwik/unused-server': 'error',
+  'qwik/jsx-img': 'error'
+};
+
+export { qwikGeneralRule };
---
+++ src/qwik.ts
@@ -0,0 +1,11 @@
+import { qwikGeneralRule } from './module/qwik/rule/qwik_general_rule';
+
+const qwik =
+{
+  plugins: ['qwik'],
+  rules: {
+    ...qwikGeneralRule
+  }
+};
+
+export { qwik };

442ae0fa3dbe2c443c93c6b9f9be6a4beb50c4e7
TheElegantCoding <luismonsalve1941@gmail.com>
2023-12-04 16:54:19 -0400

    :sparkles: feat: astro config file

--- src/module/import/rule/import_overrides.ts
+++ src/module/import/rule/import_overrides.ts
@@ -1,7 +1,7 @@
 const importOverride =
 [
   {
-    files: [ 'vite.config.ts', 'vite.config.js' ],
+    files: [ 'vite.config.ts', 'vite.config.js', 'astro.config.mjs' ],
     rules: {
       'import/no-anonymous-default-export': 'off'
     }

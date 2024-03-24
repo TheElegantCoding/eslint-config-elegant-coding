1a1e8f3d1dd7cdc4bce3f58c606c4a7a37a75761
TheElegantCoding <luismonsalve1941@gmail.com>
2024-03-23 21:21:30 -0400

    :sparkles: feat: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import elegantCoding from './dist/index.js';
 
 export default elegantCoding({
   astro: true,
+  html: true,
+  ignore: [ 'postcss.config.cjs' ],
   json: true,
+  jsxA11y: true,
   stylistic: true,
   typescript: true,
   yml: true

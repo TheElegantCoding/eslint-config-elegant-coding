db8661bab8989ec60ff20875a69a99a38ca1cb54
TheElegantCoding <luismonsalve1941@gmail.com>
2024-09-14 22:58:22 -0400

    :sparkles: feat: regex disabled rules

--- src/module/regex/rule/regex_disabled_rule.ts
+++ src/module/regex/rule/regex_disabled_rule.ts
@@ -2,7 +2,7 @@ import type { Linter } from 'eslint';
 
 const regexDisabledRule: Linter.RulesRecord =
 {
-  'regex/require-unicode-sets-regexp': 'error'
+  'regex/require-unicode-sets-regexp': 'off'
 };
 
 export { regexDisabledRule };
